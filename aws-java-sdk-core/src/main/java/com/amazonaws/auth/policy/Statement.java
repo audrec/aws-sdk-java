a3cdf039ce82e1a730f1591d15df84eee6660d51
Yi Li <liyiyi@umich.edu>
2021-12-06 23:35:51 -0800

    Revert "#2347"
    This reverts commit e69b07011a2bc228467e1d105cacb2625f929f6b.

--- aws-java-sdk-core/src/main/java/com/amazonaws/auth/policy/Statement.java
+++ aws-java-sdk-core/src/main/java/com/amazonaws/auth/policy/Statement.java
@@ -16,7 +16,10 @@ package com.amazonaws.auth.policy;
 
 import com.amazonaws.util.PolicyUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * A statement is the formal description of a single permission, and is always
@@ -410,25 +413,6 @@ public class Statement {
     public Statement withPrincipals(Principal... principals) {
         setPrincipals(principals);
         return this;
-    }
-
-    /**
-     * Compare two statements.
-     *
-     * @param obj the statement to compare with
-     *
-     * @return true if two statmenets are the same
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) { return false; }
-        if (obj == this) { return true; }
-        if (obj.getClass() != getClass()) {
-            return false;
-        }
-        Statement s1 = (Statement) obj;
-        Statement s2 = (Statement) this;
-        return Objects.equals(s1.getId(), s2.getId());
     }
 
 }
--- aws-java-sdk-sns/src/main/java/com/amazonaws/services/sns/util/Topics.java
+++ aws-java-sdk-sns/src/main/java/com/amazonaws/services/sns/util/Topics.java
@@ -29,7 +29,10 @@ import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.model.QueueAttributeName;
 import com.amazonaws.services.sqs.model.SetQueueAttributesRequest;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Set of utility methods for working with Amazon SNS topics.
@@ -186,18 +189,17 @@ public class Topics {
         String policyJson = sqsAttrs.get(QueueAttributeName.Policy.toString());
         Policy policy = extendPolicy && policyJson != null && policyJson.length() > 0
                         ? Policy.fromJson(policyJson) : new Policy();
-        Statement newStatement = new Statement(Effect.Allow)
+        policy.getStatements().add(new Statement(Effect.Allow)
                 .withId("topic-subscription-" + snsTopicArn)
                 .withPrincipals(Principal.AllUsers)
                 .withActions(SQSActions.SendMessage)
                 .withResources(new Resource(sqsQueueArn))
-                .withConditions(ConditionFactory.newSourceArnCondition(snsTopicArn));
-        if (!policy.getStatements().contains(newStatement)){
-            policy.getStatements().add(newStatement);
-        }
+                .withConditions(ConditionFactory.newSourceArnCondition(snsTopicArn)));
+
         Map<String, String> newAttrs = new HashMap<String, String>();
         newAttrs.put(QueueAttributeName.Policy.toString(), policy.toJson());
         sqs.setQueueAttributes(new SetQueueAttributesRequest(sqsQueueUrl, newAttrs));
+
         SubscribeResult subscribeResult = sns.subscribe(snsTopicArn, "sqs", sqsQueueArn);
         return subscribeResult.getSubscriptionArn();
     }
